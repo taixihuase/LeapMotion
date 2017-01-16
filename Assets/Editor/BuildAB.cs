3faee71d251356d81d8a6c931ed84e0ffdc48957
taixihuase <taixihuase@hotmail.com>
2017-01-16 18:40:13 +0800

    Create Bulid AssetBundle Editor

---
+++ Assets/Editor/BuildAB.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using Tool;
+
+namespace MyEditor
+{
+    public class BuildAB
+    {
+        [MenuItem("AssetBundle/Build Windows AssetBundle")]
+        static void BuildWinAB()
+        {
+            BuildPipeline.BuildAssetBundles(PathHelper.Instance.AssetBundlePath, BuildAssetBundleOptions.DeterministicAssetBundle, BuildTarget.StandaloneWindows64);
+            AssetDatabase.Refresh();
+        }
+    }
+}
