33c5b8a3060b12e89e8964dbec767ca94b16cb92
taixihuase <taixihuase@hotmail.com>
2017-01-19 21:22:30 +0800

    Add Main Scene With Prefabs

---
+++ Assets/Scripts/Test/TestTouch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Test
+{
+    public class TestTouch : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider obj)
+        {
+            Debug.Log(obj.transform.parent.name);
+            transform.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
