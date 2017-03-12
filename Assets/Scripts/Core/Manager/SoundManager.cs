9990827ace9431c534e6ed6db93c7db782fd18f2
taixihuase <taixihuase@hotmail.com>
2017-03-13 01:05:44 +0800

    Alter SoundManager

--- Assets/Scripts/Core/Manager/SoundManager.cs
+++ Assets/Scripts/Core/Manager/SoundManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using Boo.Lang;
+using DG.Tweening;
+using UnityEngine;
 
 namespace Core.Manager
 {
@@ -29,6 +32,18 @@ namespace Core.Manager
             effectSound = gameObject.AddComponent<AudioSource>();
         }
 
+        private void PlayEnvironmentSound(AudioClip clip, bool isLoop = false, float volumn = 1f)
+        {
+            if (clip == null)
+                return;
+
+            StopEnvironmentSound();
+            environmentSound.volume = volumn;
+            environmentSound.clip = clip;
+            environmentSound.loop = isLoop;
+            environmentSound.Play();
+        }
+
         public void PlayEnvironmentSound(string name, bool isLoop = false, float volumn = 1f)
         {
             AudioClip clip = ResourceManager.Instance.GetResource(Define.ResourceType.Sound, name) as AudioClip;
@@ -45,10 +60,7 @@ namespace Core.Manager
                 }
             }
 
-            environmentSound.volume = volumn;
-            environmentSound.clip = clip;
-            environmentSound.loop = isLoop;
-            environmentSound.Play();
+            PlayEnvironmentSound(clip, isLoop, volumn);
         }
 
         public void RestartEnvironmentSound()
@@ -67,6 +79,18 @@ namespace Core.Manager
             }
         }
 
+        private void PlayEffectSound(AudioClip clip, bool isLoop = false, float volumn = 1f)
+        {
+            if (clip == null)
+                return;
+
+            StopEffectSound();
+            effectSound.volume = volumn;
+            effectSound.clip = clip;
+            effectSound.loop = isLoop;
+            effectSound.Play();
+        }
+
         public void PlayEffectSound(string name, bool isLoop = false, float volumn = 1f)
         {
             AudioClip clip = ResourceManager.Instance.GetResource(Define.ResourceType.Sound, name) as AudioClip;
@@ -83,10 +107,7 @@ namespace Core.Manager
                 }
             }
 
-            effectSound.volume = volumn;
-            effectSound.clip = clip;
-            effectSound.loop = isLoop;
-            effectSound.Play();
+            PlayEffectSound(clip, isLoop, volumn);
         }
 
         public void RestartEffectSound()
@@ -102,6 +123,99 @@ namespace Core.Manager
             if (effectSound.clip != null)
             {
                 effectSound.Stop();
+            }
+        }
+
+        public void PlayScheduledEnvironmentSounds(List<ScheduledSound> soundClips, bool isLoopFromStart = false)
+        {
+            if(soundClips.Count == 0)
+                return;
+
+            CoroutineManager.Instance.StartCoroutine(PlayScheduledSounds(true, soundClips, isLoopFromStart));
+        }
+
+        public void PlayScheduledEffectSounds(List<ScheduledSound> soundClips, bool isLoopFromStart = false)
+        {
+            if (soundClips.Count == 0)
+                return;
+
+            CoroutineManager.Instance.StartCoroutine(PlayScheduledSounds(false, soundClips, isLoopFromStart));
+        }
+
+        IEnumerator PlayScheduledSounds(bool isEnvironment, List<ScheduledSound> soundClips, bool isLoopFromStart = false)
+        {
+            AudioClip[] clips = new AudioClip[soundClips.Count];
+            float endTime = 0;
+            int lastAvaliableClip = -1;
+            for (int i = 0; i < clips.Length; i++)
+            {
+                clips[i] = ResourceManager.Instance.GetResource(Define.ResourceType.Sound, soundClips[i].ClipName) as AudioClip;
+                if (clips[i] == null)
+                {
+                    ResourceManager.Instance.LoadAsset(Define.ResourceType.Sound, soundClips[i].ClipName, (o) =>
+                    {
+                        clips[i] = o as AudioClip;
+                    }, false, false);
+
+                    if (clips[i] == null)
+                    {
+                        continue;
+                    }
+                    lastAvaliableClip = i;
+                }
+                yield return null;
+            }
+            if (lastAvaliableClip == -1)
+                yield break;
+
+            endTime = soundClips[lastAvaliableClip].DelayTime + clips[lastAvaliableClip].length;
+            while (true)
+            {
+                float timer = 0;
+                int current = 0;
+                while (timer < endTime)
+                {
+                    if (current <= lastAvaliableClip && timer > soundClips[current].DelayTime)
+                    {
+                        if (isEnvironment)
+                        {
+                            PlayEnvironmentSound(clips[current], false, soundClips[current].Volumn);
+                        }
+                        else
+                        {
+                            PlayEffectSound(clips[current], false, soundClips[current].Volumn);
+                        }
+                        current++;
+                    }
+                    timer += Time.deltaTime;
+                    yield return new WaitForEndOfFrame();
+                }
+                if (isLoopFromStart == false)
+                {
+                    yield break;
+                }
+            }
+        }
+
+        public class ScheduledSound
+        {
+            private string clipName;
+
+            public string ClipName { get { return clipName; } }
+
+            private float delayTime;
+
+            public  float DelayTime { get { return delayTime; } }
+
+            private float volumn;
+
+            public float Volumn { get { return volumn; } }
+
+            public ScheduledSound(string clipName, float delay, float volumn = 1f)
+            {
+                this.clipName = clipName;
+                delayTime = delay;
+                this.volumn = volumn;
             }
         }
     }
